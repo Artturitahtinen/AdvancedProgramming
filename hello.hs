030d28db2c2ad9ba7dda33c43397d18dd7734100
unknown <tahtinen.artturi@gmail.com>
2020-02-19 21:50:13 +0200

    taas committi

--- hello.hs
+++ hello.hs
@@ -1,7 +1,15 @@
-multMin a b c = (max a b) * c
-square x = x * x
+repeatString str a =
+    if a == 0
+    then ""
+    else str ++ (repeatString str (a-1))
 
-posOrNeg a =
-    if a >= 0
-        then "Positive"
-        else "Negative"
+lista = [1, 5, 10]
+
+y = 10 : lista
+
+yhdistetty = lista ++ y
+
+double nums =
+    if null nums
+        then []
+        else (2 * (head nums)) : (double (tail nums))
